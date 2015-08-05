e4eae81aaf1d1a20e4821030937f25d170d51462
Feng Xi <fxi@good.com>
2015-08-05 16:41:44 +0800

    several util func

--- util.sh
+++ util.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #util functions
 
+SVC_USER="wibapp"
+
 #print INFO level log.
 function info() {
-    echo [INFO] "$1"
+    echo "[INFO]  [$(date '+%F-%T')] $1"
 }
 
 #print as an separator
@@ -13,10 +15,48 @@ function banner() {
     info "$1"
 }
 
-#print error msg in red.
+#print error msg in red to stderr.
 function err(){
-    echo -e "\e[31m[ERROR] $@\e[0m"
+    echo -e "\e[31m[ERROR] [$(date '+%F-%T')] $@\e[0m" 1>&2
 }
+
+# print error msg and exit with status code 1
+function fail-exit() {
+  err "$1"
+  exit 1
+}
+
+#print warning msg in BLUE to stdout.
+function warn(){
+    echo -e "\e[34m[WARN]  [$(date '+%F-%T')] $@\e[0m" 1>&2
+}
+
+#important reminder in red.
+function reminder(){
+    info " ------------------------------------------------------------------------"
+    info " ------------------------------------------------------------------------"
+    echo -e "\e[31m[IMPORTANT] [$(date '+%F-%T')] $@\e[0m"
+}
+
+#===  FUNCTION  ================================================================
+#          NAME:  whoami-check
+#   DESCRIPTION: check if current user is expected, usually to check if it is $SVC_USER.
+#                If not, try to call the usage() then exit 1
+#    PARAMETERS: [expected_user] , by default, it's $SVC_USER
+#       RETURNS: 1 
+#===============================================================================
+function whoami-check ()
+{
+  local expected_user="${1:-$SVC_USER}"
+  if [[ "$(whoami)" != "${expected_user}" ]]; then
+    err "Please run with user of ${expected_user}."
+    type usage >/dev/null 2>&1
+    if [[ "$?" -eq 0 ]]; then
+      usage
+    fi
+    exit 1
+  fi
+}    # ----------  end of function whoami-check  ----------
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # From http://serverfault.com/questions/177699/how-can-i-execute-a-bash-function-with-sudo/177764
